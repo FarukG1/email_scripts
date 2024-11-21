9458ac7622477e75cfdc10d51c0748965934b2bc
FarukG1 <faruk_fkg_02@outlook.de>
2024-11-21 01:17:19 +0100

    refactor main.py to improve structure, add colored text method, and consolidate email fetching logic

--- main.py
+++ main.py
@@ -27,11 +27,14 @@ class Colors:
         """Fügt Farbe zum Text hinzu."""
         return print(f"{self.__dir__[color]}{text}{self.__dir__["RESET"]}")
 
+    def colored(self, color, text):
+        """Fügt Farbe zum Text hinzu."""
+        return f"{self.__dir__[color]}{text}{self.__dir__["RESET"]}"
+
 
 class Emails:
     """Emails von einem IMAP-Server abrufen."""
 
-    filename: str = None
     host: str = None
     port: int = None
     ssl: bool = False
@@ -41,7 +44,6 @@ class Emails:
         """Initialisiert die Klasse."""
         config = configparser.ConfigParser()
         config.read("./script.ini")
-        self.filename = config["SETTINGS"]["Filename"]
         self.host = config["SETTINGS"]["Host"]
         self.port = int(config["SETTINGS"]["Port"])
         self.ssl = bool(config["SETTINGS"]["SSL"])
@@ -82,72 +84,136 @@ def open_file_explorer():
     return file_path
 
 
-# Initialize colors class
-colors = Colors()
+def main():
+    """Main function."""
+    # Initialize colors class
+    colors = Colors()
 
-# Generate initial configuration file
-if not os.path.exists("./script.ini"):
-    config = configparser.ConfigParser()
-    config["SETTINGS"] = {
-        "Filename": "",
-        "Host": "",
-        "Port": "",
-        "SSL": "",
-    }
-    config["USERS"] = {"Count": "1", "Username1": "", "Password1": ""}
-    with open("./script.ini", "w", encoding="utf-8") as configfile:
-        config.write(configfile)
+    # Generate initial configuration file
+    if not os.path.exists("./script.ini"):
+        config = configparser.ConfigParser()
+        config["SETTINGS"] = {
+            "host": "",
+            "port": "",
+            "ssl": "",
+        }
+        config["USERS"] = {"count": "1", "username1": "", "password1": ""}
+        with open("./script.ini", "w", encoding="utf-8") as configfile:
+            config.write(configfile)
+        colors.print(
+            "ERROR",
+            "Please fill out the configuration file (script.ini) and run the script again.",
+        )
+        return 0
+
+    # Initialize emails class
+    emails = Emails()
+
+    # Fetch emails for each user
+    sent_addresses = []
+    inbox_addresses = []
+    for user in emails.users:
+        colors.print("CYAN", f"Fetching emails for {user[0]}...")
+        sent_mails, inbox_mails = emails.fetch_one_user(user[0], user[1])
+
+        # Fetch sent emails
+        # colors.print("CYAN", "Fetching sent emails...")
+        for _, data in sent_mails.items():
+            envelope = data[b"ENVELOPE"]
+            address = (
+                envelope.to[0].mailbox.decode(encoding="utf-8")
+                + "@"
+                + envelope.to[0].host.decode(encoding="utf-8")
+            )
+            sent_addresses.append(address)
+
+        # Fetch inbox emails
+        # colors.print("CYAN", "Fetching inbox emails...")
+        for _, data in inbox_mails.items():
+            envelope = data[b"ENVELOPE"]
+            address = (
+                envelope.from_[0].mailbox.decode(encoding="utf-8")
+                + "@"
+                + envelope.from_[0].host.decode(encoding="utf-8")
+            )
+            inbox_addresses.append(address)
+
+    # all email adresses to lowercase
+    sent_addresses = [address.lower() for address in sent_addresses]
+    inbox_addresses = [address.lower() for address in inbox_addresses]
+
+    # Print count of sent and inbox emails
     colors.print(
-        "ERROR",
-        "Please fill out the configuration file (script.ini) and run the script again.",
+        "PURPLE", "Sent Email Addresses Count: " + str(len(set(sent_addresses)))
     )
-    exit(0)
-
-# Initialize emails class
-emails = Emails()
-
-# Fetch emails for each user
-sent_addresses = []
-inbox_addresses = []
-for user in emails.users:
-    colors.print("CYAN", f"Fetching emails for {user[0]}...")
-    sent_mails, inbox_mails = emails.fetch_one_user(user[0], user[1])
-
-    # Fetch sent emails
-    colors.print("CYAN", "Fetching sent emails...")
-    for msgid, data in sent_mails.items():
-        envelope = data[b"ENVELOPE"]
-        address = (
-            envelope.to[0].mailbox.decode(encoding="utf-8")
-            + "@"
-            + envelope.to[0].host.decode(encoding="utf-8")
-        )
-        sent_addresses.append(address)
-
-    # Fetch inbox emails
-    colors.print("CYAN", "Fetching inbox emails...")
-    for msgid, data in inbox_mails.items():
-        envelope = data[b"ENVELOPE"]
-        address = (
-            envelope.from_[0].mailbox.decode(encoding="utf-8")
-            + "@"
-            + envelope.from_[0].host.decode(encoding="utf-8")
-        )
-        inbox_addresses.append(address)
-
-# Print count of sent and inbox emails
-colors.print("GREEN", "Sent Count: " + str(len(set(sent_addresses))))
-colors.print("ERROR", "Inbox Count: " + str(len(set(inbox_addresses))))
-
-count = 0
-csv_rows = []
-for address in set(sent_addresses).difference(set(inbox_addresses)):
-    csv_rows.append([address])
-    count += 1
-with open(emails.filename, "w", newline="", encoding="utf-8") as csvfile:
-    writer = csv.writer(
-        csvfile, delimiter=",", quotechar="|", quoting=csv.QUOTE_MINIMAL
+    colors.print(
+        "PURPLE", "Inbox Email Addresses Count: " + str(len(set(inbox_addresses)))
     )
-    writer.writerow(["Email"])
-    writer.writerows(csv_rows)
-colors.print("CYAN", f"Total No-Replys: {count + 1}")
+
+    count = 0
+    csv_rows = []
+    for address in set(sent_addresses).difference(set(inbox_addresses)):
+        csv_rows.append([address])
+        count += 1
+    colors.print("PURPLE", f"Total No-Replys Email Addresses: {count + 1}\n\n")
+
+    selected_file = open_file_explorer()
+
+    if not selected_file:
+        colors.print("ERROR", "No file selected.")
+        input(colors.colored("ERROR", "Press Enter to exit and try again..."))
+        return 0
+
+    colors.print(
+        "GREEN",
+        f'Selected File: "\x1b]8;;file://{selected_file}/\x1b\\{selected_file}\x1b]8;;\x1b\\"',
+    )
+
+    colors.print("GREEN", "Reading CSV file...")
+    csv_rows = []
+    with open(selected_file, "r", encoding="utf-8") as csvfile:
+        csv_reader = csv.reader(csvfile)
+        email_index = 0
+        for row in csv_reader:
+            for index, column in enumerate(row):
+                if "email" in column.lower():
+                    email_index = index
+                    break
+            break
+
+        for row in csv_reader:
+            if row[email_index] in set(sent_addresses).difference(set(inbox_addresses)):
+                csv_rows.append(row)
+
+    filename = selected_file.split(".csv")[0] + "_no_reply.csv"
+    colors.print(
+        "GREEN",
+        f'Writing CSV file to "\x1b]8;;file://{filename}/\x1b\\{filename}\x1b]8;;\x1b\\"',
+    )
+    with open(filename, "w", newline="", encoding="utf-8") as csvfile:
+        writer = csv.writer(
+            csvfile, delimiter=",", quotechar="|", quoting=csv.QUOTE_MINIMAL
+        )
+        writer.writerow(
+            [
+                "Name",
+                "Handle",
+                "Link",
+                "Followers",
+                "Gender",
+                "Email",
+                "Location",
+                "",
+                "",
+                "",
+            ]
+        )
+        writer.writerows(csv_rows)
+
+    print("\n")
+    input(colors.colored("GREEN", "Press Enter to exit..."))
+    return 0
+
+
+if __name__ == "__main__":
+    main()
